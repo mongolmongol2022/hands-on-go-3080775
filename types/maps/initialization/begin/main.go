f45dadf32baf0a813fa34b76f6d7b0af4840c396
mongolmongol2022 <103179197+mongolmongol2022@users.noreply.github.com>
2023-01-13 12:15:12 +0000

    @@@Chap.3 is done

--- types/maps/initialization/begin/main.go
+++ types/maps/initialization/begin/main.go
@@ -1,23 +1,35 @@
 // types/maps/initialization/begin/main.go
 package main
 
+import "fmt"
+
 type author struct {
 	name string
 }
 
 func main() {
 	// declare a map of string keys and author values
-	//
 
-	// initialize the map with make
-	//
+	// var authors map[string]author
+	// authors = make(map[string]author)
+	// authors["tm"] = author{name: "Toni Morrison"}
+	// authors["ma"] = author{name: "Marcus Aurelius"}
 
-	// add authors to the map
-	//
+	// authors := map[string]author{
+	// 	"tm": author{name: "Toni Morrison"},
+	// 	"ma": author{name: "Marcus Aurelius"},
+	// }
+
+	authors := map[string]author{
+		"tm": {name: "Toni Morrison"},
+		"ma": {name: "Marcus Aurelius"},
+	}
 
 	// print the map with fmt.Printf
-	//
+	// fmt.Printf("%#v\n", authors)
+	// fmt.Printf("%v\n", authors)
 
 	// read a value from the map with a known key
-	//
+	fmt.Println(authors["tm"])
+
 }
